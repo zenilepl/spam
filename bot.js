7f424e540c709c90c8a377b766a9b5566223fcca
zenilepl <45181384+zenilepl@users.noreply.github.com>
2018-11-23 19:45:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,6 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-let channel =     client.channels.get("513473609713319941")
+let channel =     client.channels.get("515583737656901679")
 
 client.login(process.env.BOT_TOKEN);
